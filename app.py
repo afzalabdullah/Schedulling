29b574e569a2a7e3110d66ace96f0b4f0feda3fe
Abdullah Afzal <98462344+afzalabdullah@users.noreply.github.com>
2024-05-24 17:54:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import random as rnd
 from datetime import datetime, timedelta
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Constants for Event Scheduling
 NUMB_OF_ELITE_EVENTS = 2
